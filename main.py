2d3d9ad569dc683fe229449c42c11c0e8ad0b369
Brian <b.mwololo@alustudent.com>
2025-11-24 16:12:44 +0400

    added main function

--- main.py
+++ main.py
@@ -8,3 +8,35 @@ def show_menu():
     print("4) Filter Transactions")
     print("5) Show Summary")
     print("0) Exit")
+
+def main():
+    tracker = BudgetTracker()
+
+    while True:
+        show_menu()
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            tracker.add_income()
+
+        elif choice == "2":
+            tracker.add_expense()
+
+        elif choice == "3":
+            tracker.list_transactions()
+
+        elif choice == "4":
+            tracker.filter_transactions()
+
+        elif choice == "5":
+            tracker.show_summary()
+
+        elif choice == "0":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
