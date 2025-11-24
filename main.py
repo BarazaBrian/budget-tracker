2cb53208cbe89779e9fe1fc632d8c3f9ed2bba35
Brian <b.mwololo@alustudent.com>
2025-11-24 16:11:54 +0400

    added show_menu function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from tracker import BudgetTracker
+
+def show_menu():
+    print("\n===== Budget Tracker =====")
+    print("1) Add Income")
+    print("2) Add Expense")
+    print("3) List Transactions")
+    print("4) Filter Transactions")
+    print("5) Show Summary")
+    print("0) Exit")
